428d1b6119b1db9b3aef6b8a20b8d591c890858f
Venticinque Mauro <mauro.venticinque@hotmail.com>
2022-09-30 12:52:09 -0300

    error en colores links

--- coder-app/src/components/Presentacion/Presentacion.jsx
+++ coder-app/src/components/Presentacion/Presentacion.jsx
@@ -12,10 +12,10 @@ export const Presentacion = ({ ingresar }) => {
                     <h3 className='textoSubtitulo animate__animated animate__fadeIn animate__delay-2s' style={{textShadow: '-2px 3px 5px #000'}}><span className='comillas'>“</span> Too much of anything is bad, but too much good whiskey is rarely enough <span className='comillas'>”</span></h3>
                     <div className="row mt-3">
                         <div className="col-6 text-end animate__animated animate__fadeIn animate__delay-3s">
-                            <a href="https://www.linkedin.com/in/mauro-venticinque-39a256235/" target='_blank'><i className="fa-brands fa-linkedin-in iconoPresentacion h4 p-2"></i></a>
-                            <a href="https://github.com/VenticinqueMauro" target='_blank'><i className="fa-brands fa-github-alt iconoPresentacion h4 p-2"></i></a>
-                            <a href="https://twitter.com/VenticinqueMa" target='_blank'><i className="fa-brands fa-twitter iconoPresentacion h4 p-2"></i></a>
-                            <a href="mailto:mauro.venticinque@hotmail.com"><i className="fa-solid fa-envelope iconoPresentacion h4 p-2"></i></a>
+                            <a href="https://www.linkedin.com/in/mauro-venticinque-39a256235/" target='_blank' className='text-white'><i className="fa-brands fa-linkedin-in iconoPresentacion h4 p-2"></i></a>
+                            <a href="https://github.com/VenticinqueMauro" target='_blank' className='text-white'><i className="fa-brands fa-github-alt iconoPresentacion h4 p-2"></i></a>
+                            <a href="https://twitter.com/VenticinqueMa" target='_blank' className='text-white'><i className="fa-brands fa-twitter iconoPresentacion h4 p-2"></i></a>
+                            <a href="mailto:mauro.venticinque@hotmail.com" className='text-white'><i className="fa-solid fa-envelope iconoPresentacion h4 p-2"></i></a>
                         </div>
                         <div className="col-6 text-start">
                         <button className='textoBoton btn btn-warning animate__animated animate__fadeIn animate__delay-3s' onClick={() => ingresar()}>Come Here!</button>
